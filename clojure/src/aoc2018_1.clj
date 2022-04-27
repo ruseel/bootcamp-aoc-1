4ddf0a5e077a95f57b792e6a66c4892cac19a973
ruseel <ruseel@gmail.com>
2022-04-27 13:26:22 +0900

    review
    - thread-first, thread-last 섞는 것 없이 좀 더 의도가 잘 드러나게
    - loop recur 대신 reduce + reduced 로 구현
    - (reduce + ,,,) 를 (apply + ,,,) 로 변경

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -5,18 +5,43 @@
 ;; 주어진 입력의 모든 숫자를 더하시오.
 ;; 예) +10 -2 -5 +1 이 입력일 경우 4를 출력
 
+;; lines 를 let 으로 바꿉니다.
+;; thread-first 와 thread-last 를 섞지 않도록 하기 위한 첫 시도.
 (def vs
-  (-> (io/resource "day1.txt")
-      slurp
-      (clojure.string/split #"\n+")
-      (->> (map #(Integer/parseInt %)))))
+  (let [lines (-> (io/resource "day1.txt")
+                  slurp
+                  (clojure.string/split #"\n+"))]
+    (map #(Integer/parseInt %) lines)))
 
-(reduce + vs)
+(apply + vs)
 
 ;; 파트 2
 ;; 주어진 입력의 숫자를 더할 때 마다 나오는 숫자 중, 처음으로 두번 나오는 숫자를 리턴하시오.
 ;; 예) +3, +3, +4, -2, -4 는 10이 처음으로 두번 나오는 숫자임.
 ;; 0 -> 3 (+3) -> 6 (+3) -> 10(+4) -> 8(-2) -> 4(-4) -> 7(+3) -> 10(+3) -> ...
+
+;; destructuring, cycle, cons
+;;   destructuring, cycle 은 확실히 사용하는 것이 더 좋은 것이다 싶은데요.
+;;   cons 는 좀 애매하다 싶습니다.
+;;
+;; f & rest 라는 변수명을 사용하다가
+;; review 후에 v & more 로 바꿨습니다.
+(def find-first-dup-1 [frequencies]
+  (loop [seen #{}
+         [v & more] frequencies]
+    (if (seen v)
+      v
+      (recur (conj seen v) more))))
+
+;; reduce 를 사용하자
+(def find-first-dup-2 [frequencies]
+  (let [[_ first-dup] (reduce (fn [[seen first-dup] v]
+                                (if (seen v)
+                                  (reduced [seen v])
+                                  [(conj seen v) nil]))
+                              [#{} nil]
+                              frequencies)]
+    first-dup))
 
 (comment
 
@@ -25,21 +50,6 @@
   (def vs [+3, +3, +4, -2, -4])
   (def vs [-6, +3, +8, +5, -6])
 
-)
+  (reductions + (cons 0 (reductions + (cycle vs))))
 
-;; first, next 를 사용한 버전
-(loop [seen #{}
-       vs (concat [0] (reductions + (flatten (repeat vs))))]
-  (let [v (first vs)]
-    (if (seen v)
-      v
-      (recur (conj seen v) (next vs)))))
-
-;; destructuring, cycle, cons
-;;   destructuring, cycle 은 확실히 사용하는 것이 더 좋은 것이다 싶은데요.
-;;   cons 는 좀 애매하다 싶습니다.
-(loop [seen #{}
-       [f & rest] (cons 0 (reductions + (cycle vs)))]
-  (if (seen f)
-    f
-    (recur (conj seen f) rest)))
+  )

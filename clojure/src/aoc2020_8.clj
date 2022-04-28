225072ed3836e2903d83af163246569ca5d9a0b6
ruseel <ruseel@gmail.com>
2022-04-28 14:37:22 +0900

    이제 문제 정의대로 돌아간다
    - 버그를 고쳤는데 instruction pointer 에 위치한 instruction 을
      실행하지 않고 있었다.

--- clojure/src/aoc2020_8.clj
+++ clojure/src/aoc2020_8.clj
@@ -5,16 +5,11 @@
 (defn- parse-int [s]
   (Integer/parseInt s))
 
-(def lines (-> "day8.txt"
-               io/resource
-               io/reader
-               line-seq))
-
 (defn line->instruction [s]
   (let [[a b] (split s #"\s+" 2)]
     {:type :instruction
      :op (keyword a)
-     :operand b}))
+     :operand (parse-int b)}))
 
 ;; ip - instruction pointer
 ;; acc - accumulator register
@@ -25,52 +20,98 @@
 (defmulti eval-ins (fn [env instruction] (:op instruction)) :default nil)
 
 (defmethod eval-ins :acc [env instruction]
-  (let [v (parse-int (:operand instruction))]
+  (let [v (:operand instruction)]
     (-> env
         (update-in [:acc] (partial + v))
         (update-in [:ip] inc))))
 
 (defmethod eval-ins :jmp [env instruction]
-  (let [v (parse-int (:operand instruction))]
+  (let [v (:operand instruction)]
     (-> env
         (update-in [:ip] (partial + v)))))
 
-(defmethod eval-ins :nop [env _] env)
+(defmethod eval-ins :nop [env _]
+  (-> env (update-in [:ip] inc)))
 
+(defn record-visited-addrs [env]
+  (let [ip (:ip env)]
+    (-> env
+        (update-in [:visited-addrs] #(conj % ip)))))
 
 (defn- infinite-loop? [env]
   (contains? (:visited-addrs env) (:ip env)))
 
-(defn- update-visited-addrs [{ip :ip :as env}]
-  (update-in env [:visitied-addrs] #(conj % ip)))
-
 ;; loop-recur 를 썼습니다.
 ;; reduce 로 바꾸는 것이 영 어색해서
 ;; 그건 좀 더 작업하도록 해볼께요.
-(defn eval-print-loop [env instructions]
-  (loop [env env
-         [ins & more] instructions]
-    (let [env (-> env
-                  (eval-ins ins)
-                  update-visited-addrs)]
-      (prn [ins env])
-      (if (infinite-loop? env)
-        (throw (ex-info "infinite loop")))
-      (if (seq? more)
-        (recur env more)))))
+
+(defn- step [env ins]
+  (-> env
+      record-visited-addrs
+      (eval-ins ins)))
+
+(defn eval [env instructions]
+  (loop [env env]
+    (cond
+      (infinite-loop? env)
+      (assoc env :halt :detected-infinite-loop)
+
+      (>= (:ip env) (count instructions))
+      (assoc env :halt :finished)
+
+      :else
+      (recur (step env (nth instructions (:ip env)))))))
 
 (comment
 
-  (ns-unmap *ns* 'eval)
+  (ns-unmap *ns* 'eval-ins)
 
-  (def instructions
-    (map line->instruction lines))
+  (eval-ins env {:op :acc :operand +3})
+  (eval-ins env {:op :jmp :operand +4})
+  (eval-ins env {:op :nop :operand +0})
+  (eval-ins env {:op :nop :operand 0})
 
-  (eval-ins env {:op :acc :operand "+3"})
-  (eval-ins env {:op :jmp :operand "+4"})
-  (eval-ins env {:op :nop :operand "+0"})
-  (eval-ins env {:op :nop :operand "+x"})
+  (eval env
+        (map line->instruction
+             ["nop +1",
+              "nop +1"
+              "nop +1"
+              "nop +1"
+              "nop +1"]))
 
-  (eval-print-loop env instructions)
+  (eval env (map line->instruction
+                 ["nop +0"
+                  "jmp -1"
+                  "nop +0"]))
 
+  (def sample-instructions (map line->instruction
+                  ["nop +0"
+                   "jmp -1"]))
+
+  (-> env
+      ((fn [env] (prn (infinite-loop? env)) env))
+      (step (nth sample-instructions 0))
+      ((fn [env] (prn (infinite-loop? env)) env))
+      (step (nth sample-instructions 1))
+      ((fn [env] (prn (infinite-loop? env)) env)))
+
+  (eval env (map line->instruction
+                 ["nop +0"
+                  "acc +1"
+                  "jmp +4"
+                  "acc +3"
+                  "jmp -3"
+                  "acc -99"
+                  "acc +1"
+                  "jmp -4"
+                  "acc +6"]))
+
+  (def instructions (map line->instruction lines))
+
+  (def lines (-> "day8.txt"
+                 io/resource
+                 io/reader
+                 line-seq))
+
+  (eval env instructions)
   )

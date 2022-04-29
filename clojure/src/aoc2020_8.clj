7a28d118fea0a33e28b2b4f06fcd64a6c77d8e71
ruseel <ruseel@gmail.com>
2022-04-29 10:33:57 +0900

    drop-while + iterate

--- clojure/src/aoc2020_8.clj
+++ clojure/src/aoc2020_8.clj
@@ -11,32 +11,41 @@
      :op (keyword a)
      :operand (parse-int b)}))
 
+(def lines (-> "day8.txt"
+               io/resource
+               io/reader
+               line-seq))
+
+(def instructions (map line->instruction lines))
+
 ;; ip - instruction pointer
 ;; acc - accumulator register
-(def env {:ip 0 :acc 0 :visited-addrs #{}})
+(def env {:ip 0
+          :acc 0
+          :visited-addrs #{}
+          :instructions instructions})
 
-;; eval-ins
+;; execute-ins
 ;;   (env, instruction) -> env
-(defmulti eval-ins (fn [env instruction] (:op instruction)) :default nil)
+(defmulti execute-ins (fn [env instruction] (:op instruction)) :default nil)
 
-(defmethod eval-ins :acc [env instruction]
+(defmethod execute-ins :acc [env instruction]
   (let [v (:operand instruction)]
     (-> env
         (update-in [:acc] (partial + v))
         (update-in [:ip] inc))))
 
-(defmethod eval-ins :jmp [env instruction]
+(defmethod execute-ins :jmp [env instruction]
   (let [v (:operand instruction)]
     (-> env
         (update-in [:ip] (partial + v)))))
 
-(defmethod eval-ins :nop [env _]
+(defmethod execute-ins :nop [env _]
   (-> env (update-in [:ip] inc)))
 
-(defn record-visited-addrs [env]
-  (let [ip (:ip env)]
-    (-> env
-        (update-in [:visited-addrs] #(conj % ip)))))
+(defn record-visited-addrs [env ip]
+  (-> env
+      (update-in [:visited-addrs] #(conj % ip))))
 
 (defn- infinite-loop? [env]
   (contains? (:visited-addrs env) (:ip env)))
@@ -45,73 +54,48 @@
 ;; reduce 로 바꾸는 것이 영 어색해서
 ;; 그건 좀 더 작업하도록 해볼께요.
 
-(defn- step [env ins]
-  (-> env
-      record-visited-addrs
-      (eval-ins ins)))
+(defn- step [{ip :ip instructions :instructions :as env}]
+  (cond
+    (infinite-loop? env)
+    (assoc env :halt :detected-infinite-loop)
 
-(defn eval [env instructions]
-  (loop [env env]
-    (cond
-      (infinite-loop? env)
-      (assoc env :halt :detected-infinite-loop)
+    (>= ip (count instructions))
+    (assoc env :halt :finished)
 
-      (>= (:ip env) (count instructions))
-      (assoc env :halt :finished)
+    :else
+    (-> env
+        (record-visited-addrs ip)
+        (execute-ins (nth instructions ip)))))
 
-      :else
-      (recur (step env (nth instructions (:ip env)))))))
+(defn execute [env]
+  (iterate step-1 env))
+
+(defn solve [env]
+  (first (drop-while #(nil? (:halt %)) (execute env))))
 
 (comment
 
-  (ns-unmap *ns* 'eval-ins)
+  (execute-ins env {:op :acc :operand +3})
+  (execute-ins env {:op :jmp :operand +4})
+  (execute-ins env {:op :nop :operand +0})
+  (execute-ins env {:op :nop :operand 0})
 
-  (eval-ins env {:op :acc :operand +3})
-  (eval-ins env {:op :jmp :operand +4})
-  (eval-ins env {:op :nop :operand +0})
-  (eval-ins env {:op :nop :operand 0})
+  (execute (merge env
+                  {:instructions (map line->instruction
+                                      ["nop +1",
+                                       "nop +1"
+                                       "nop +1"
+                                       "nop +1"
+                                       "nop +1"])}))
 
-  (eval env
-        (map line->instruction
-             ["nop +1",
-              "nop +1"
-              "nop +1"
-              "nop +1"
-              "nop +1"]))
+  (execute (merge env
+                  {:instructions (map line->instruction
+                                          ["nop +0"
+                                           "jmp -1"
+                                           "nop +0"])}))
 
-  (eval env (map line->instruction
-                 ["nop +0"
-                  "jmp -1"
-                  "nop +0"]))
+  (eval (merge env {:instructions instructions}))
 
-  (def sample-instructions (map line->instruction
-                  ["nop +0"
-                   "jmp -1"]))
+  (first (drop-while #(nil? (:halt %)) (execute env)))
 
-  (-> env
-      ((fn [env] (prn (infinite-loop? env)) env))
-      (step (nth sample-instructions 0))
-      ((fn [env] (prn (infinite-loop? env)) env))
-      (step (nth sample-instructions 1))
-      ((fn [env] (prn (infinite-loop? env)) env)))
-
-  (eval env (map line->instruction
-                 ["nop +0"
-                  "acc +1"
-                  "jmp +4"
-                  "acc +3"
-                  "jmp -3"
-                  "acc -99"
-                  "acc +1"
-                  "jmp -4"
-                  "acc +6"]))
-
-  (def instructions (map line->instruction lines))
-
-  (def lines (-> "day8.txt"
-                 io/resource
-                 io/reader
-                 line-seq))
-
-  (eval env instructions)
   )
